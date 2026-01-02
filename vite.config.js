b3c4a1c10276ee388e09e10502516797a3ec099b
Aditya Suryawanshi <adityasuryawanshi038@gmail.com>
2026-01-02 15:21:47 +0530

    Fix contribution grid layout and remove hover tooltip

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: true,
+      enabled: false,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
